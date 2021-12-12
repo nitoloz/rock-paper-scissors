2a872cec011b9fef763294203ddb14f79abeb6c1
andreyzolotin <a.zolotin@cioplenu.de>
2021-12-12 16:27:49 +0100

    Auto-generated commit [ci skip]

--- src/app/ngrx/actions/app.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Hand } from '../../models/hand';
-
-export const changeGameType = createAction('[RPS] Change game type');
-export const resetScore = createAction('[RPS] Reset game score');
-export const toggleRulesPopup = createAction('[RPS] Toggle rules popup');
-export const selectHand = createAction('[RPS] Select hand for player', props<{ hand: Hand }>());
-export const playAgain = createAction('[RPS] Play again');
